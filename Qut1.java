693e9497dfa315c6ac52506f3e7d09d7177d006f
madhu86280 <madhubanga22@gmail.com>
2018-10-03 22:36:14 -0700

    collection3

---
+++ Qut1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+ class Qut1
+ {
+
+	public static void main(String[] args) 
+     {
+     ArrayList<String> s=new ArrayList<String>();
+     s.add("madhurajput");
+     s.add("vihan");
+     s.add("malhotra");
+     Collections.sort(s);
+     System.out.println(s);
+
+     ArrayList<Integer> i=new ArrayList<Integer>();
+
+     i.add(10);
+     i.add(200);
+     i.add(3);
+     Collections.sort(i);
+     System.out.println(i);
+	}
+
+}
